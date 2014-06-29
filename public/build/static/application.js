51becb1ee7752f7d8c5a3016a98b1e1559ab54dd
Samy Pessé <samypesse@gmail.com>
2014-06-29 23:28:31 +0200

    Add base model for notifications

--- public/build/static/application.js
+++ public/build/static/application.js
@@ -25933,7 +25933,7 @@ Logger, Requests, Urls, Storage, Cache, Cookies, Template, Resources, Offline, B
     
     return hr;
 });
-define('hr/args',[],function() { return {"revision":1404049299940,"baseUrl":"/"}; });
+define('hr/args',[],function() { return {"revision":1404077279748,"baseUrl":"/"}; });
 define('core/api',[
     'hr/hr'
 ], function(hr) {
