6e94081fb96aea9bed57a0361cce4cd70ff231da
Samy Pessé <samypesse@gmail.com>
2014-07-02 13:27:30 +0200

    Reduce font size in header

--- public/build/static/application.js
+++ public/build/static/application.js
@@ -25933,7 +25933,7 @@ Logger, Requests, Urls, Storage, Cache, Cookies, Template, Resources, Offline, B
     
     return hr;
 });
-define('hr/args',[],function() { return {"revision":1404297260220,"baseUrl":"/"}; });
+define('hr/args',[],function() { return {"revision":1404300427806,"baseUrl":"/"}; });
 define('core/api',[
     'hr/hr'
 ], function(hr) {
